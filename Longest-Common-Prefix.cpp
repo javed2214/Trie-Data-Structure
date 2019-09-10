8b395391ee22a1b73dcc147c2ff0b407830280b4
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-09-10 19:12:22 +0530

    Add files via upload

---
+++ Longest-Common-Prefix.cpp
@@ -0,0 +1,61 @@
+// Program to Find Longest Common Prefix Using Trie Data Structure
+// https://leetcode.com/problems/longest-common-prefix/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef struct TrieNode{
+
+	unordered_map<char, TrieNode *> Map;
+	bool isEndOfWord;
+
+}TrieNode;
+
+TrieNode *getNewTrieNode(){
+
+	TrieNode *node = new TrieNode;
+	node->isEndOfWord = false;
+
+	return node;
+}
+
+TrieNode *insertWords(TrieNode *root, vector <string> &v){
+
+	for(int i=0;i<v.size();i++){
+
+		string s = v[i];
+		if(root == NULL) root = getNewTrieNode();
+		TrieNode *temp = root;
+		for(auto it:s){
+			if(temp->Map.find(it) == temp->Map.end()) temp->Map[it] = getNewTrieNode();
+			temp = temp->Map[it];
+		}
+		temp->isEndOfWord = true;
+	}
+	return root;
+}
+
+string LCP(TrieNode *root){
+
+	string s;
+	TrieNode *node = root;
+
+	while(node and !node->isEndOfWord and node->Map.size() == 1){
+
+		auto it = node->Map.begin();
+		s += it->first;
+		node = it->second;
+	}
+	return s;
+}
+
+int main(){
+
+	vector <string> v = {"coder", "coding", "codezen", "codeblocks"};
+	TrieNode *root = NULL;
+	root = insertWords(root, v);
+
+	cout<<LCP(root);
+
+	return 0;
+}
